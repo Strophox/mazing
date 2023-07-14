6175a03595fed1a0ebaf1d4602edc11bb5c257f6
Strophox <strophox@gmail.com>
2023-07-15 00:07:22 +0200

    add utf_half, rename str functions

--- maze.py
+++ maze.py
@@ -12,7 +12,6 @@ Work in Progress:
   * Recursive division
 - Printers:
   * utf_thin
-  * utf_half: ▀▯▄▯█
   * utf_quarter: ▘▯▝▯▀▯▖▯▌▯▞▯▛▯▗▯▚▯▐▯▜▯▄▯▙▯▟▯█
 - ETC Dreams:
   * Maze navigator (w/ curses)
@@ -54,11 +53,11 @@ class Maze:
     def __repr__(self):
         return "[" + ',\n'.join(map(repr,self.grid)) + "]"
 
-    def __format__(self, *args):
-        if args[0] == '':
-            return self.ascii_thin()
-        else:
-            return self.ascii_block(wall=args[0])
+    #def __format__(self, *args):
+        #if args[0] == '':
+            #return self.ascii_thin()
+        #else:
+            #return self.ascii_block(wall=args[0])
 
     def bitmap(self, columnated=True):
         has_wall, [RIGHT,UP,LEFT,DOWN] = self.has_wall, self.DIRECTIONS
@@ -90,13 +89,18 @@ class Maze:
     def ascii_block(self, wall=None, air=None):
         """
         Produce a canonical, 'blocky' ASCII representation of the maze.
+        Keyword arguments `wall`/`air` may also be functions that produce random texture instead of a fixed string.
         """
-        if wall is None:
-            wall = '%#'
-        if air is None:
-            air = len(wall)*' '
+        # Sort out non-/default wall/air texture,
+        if wall is None: make_wall = lambda: '##'
+        elif callable(wall): make_wall = wall
+        else: make_wall = lambda: wall
+        if air is None: make_air = lambda: ' '*len(make_wall())
+        elif callable(air): make_air = air
+        else: make_air = lambda: air
+        # Actually produce string using default maze bitmap
         bmap = self.bitmap(columnated=True)
-        string = '\n'.join(''.join(wall if b else air for b in row) for row in bmap)
+        string = '\n'.join(''.join(make_wall() if b else make_air() for b in row) for row in bmap)
         return string
 
     def ascii_thin(self):
@@ -151,6 +155,23 @@ class Maze:
                 string += cornersegment(x,y)
         return string
 
+    def utf_block(self):
+        return self.ascii_block(wall='██',air='  ')
+
+    def utf_half(self):
+        """
+        Produce blocky unicode art to represent the maze, at half the size.
+        """
+        tiles = " ▄▀█"
+        bmap = self.bitmap(columnated=True)
+        if len(bmap)%2!=0:
+            bmap.append([False for _ in bmap[0]])
+        string = ""
+        for y in range(0,len(bmap),2):
+            string += '\n'
+            string += ''.join(tiles[2*hi + 1*lo] for (hi,lo) in zip(bmap[y],bmap[y+1]))
+        return string
+
     def utf_pipe(self):
         """
         Produce pipe-like unicode art to represent the maze.
@@ -173,9 +194,11 @@ class Maze:
         return string
 
     def edge_toward(self, node_coordinate, direction): # TODO bad idea?..
-        """Check whether there is an edge from a specified node into some direction.
+        """
+        Check whether there is an edge from a specified node into some direction.
         - node_coordinate : (x,y) where 0<=x<width && 0<=y<height
-        - direction : one of self.DIRECTIONS"""
+        - direction : one of self.DIRECTIONS
+        """
         (x,y) = node_coordinate
         return bool(self.grid[y][x] & direction)
 
@@ -197,10 +220,12 @@ class Maze:
 
 def main():
     maze = Maze(10,10)
-    print("repr : \n" + str(maze))
-    print(f"ascii_thin : \n{maze}")
-    print(f"ascii_block : \n{maze:██}")
-    print(f"utf_pipe : \n{maze.utf_pipe()}")
+    print(f"repr:\n{repr(maze)}")
+    print(f"ascii_block:\n{maze.ascii_block(wall=lambda:random.choice(['##','#@','%#']))}")
+    print(f"ascii_thin:\n{maze.ascii_thin()}")
+    print(f"utf_block:\n{maze.utf_block()}")
+    print(f"utf_half:\n{maze.utf_half()}")
+    print(f"utf_pipe:\n{maze.utf_pipe()}")
 
     #help(Maze)
 
