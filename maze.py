d51a61b0f7539803e9511c9bc0973c64e82710ab
Strophox <strophox@gmail.com>
2023-07-14 15:33:59 +0200

    fix ascii_str

--- maze.py
+++ maze.py
@@ -3,12 +3,17 @@
 
 # A small script to generate some mazes
 
+# TO DO
+# - █▯▓▯▒▯░▯ ▯▘▯▝▯▀▯▖▯▌▯▞▯▛▯▗▯▚▯▐▯▜▯▄▯▙▯▟▯█
+# - ╶╺╵└┕╹┖┗╴─╼┘┴┶┚┸┺╸╾━┙┵┷┛┹┻╷┌┍│├┝╿┞┡┐┬┮┤┼┾┦╀╄┑┭┯┥┽┿┩╃╇╻┎┏╽┟┢┃┠┣┒┰┲┧╁╆┨╂╊┓┱┳┪╅╈┫╉╋
+
 # OUTLINE END
 
 
 # IMPORTS BEGIN
 
 import random
+import pprint
 
 # IMPORTS END
 
@@ -25,13 +30,16 @@ class Maze:
         assert(width_ > 0 and height_ > 0)
         self.width = width_
         self.height = height_
-        self.grid = [[random.randint(0,8) for _ in range(width_)] for _ in range(height_)]
+        self.grid = [[random.randint(0b0000,0b1111) for _ in range(width_)] for _ in range(height_)]
+
+    def __repr__(self):
+        return pprint.pformat(self.grid)
 
     def __format__(self, *args):
         if args[0]=='':
             return self.ascii_thin()
         else:
-            return self.ascii_str(args[0])
+            return self.ascii_str(wall=args[0])
 
     def ascii_thin(self):
         """Produce a compact ASCII representation."""
@@ -48,10 +56,10 @@ class Maze:
 #,___, ,___, ,___, ,___,
 #|_, | |___| |_| | |_|_|
 #|_|_| |_|_| |_|_| |_|_|
-        string = f",{'_'*(2*self.width-1)}," # Top fence
+        string = f",{'_'*(2*self.width-1)}," # Top fence # FIXME correct holes
         # Add to string row-wise
         for y,row in enumerate(self.grid):
-            string += "\n|" # Left fence
+            string += "\n|" # Left fence # FIXME correct holes
             # Add two chars per node
             for x,node in enumerate(row):
                 # Decide middle segment
@@ -73,16 +81,20 @@ class Maze:
         return string
 
     def ascii_str(self, wall=None, air=None):
+        """Produce a 'block' ASCII representation of the maze."""
         if wall is None: wall = '%#'
         if air is None: air = len(wall)*' '
-        string = f"{wall*(2*self.width+1)}"
+        # Produce topmost row
+        string = f"{wall}" + ''.join(f"{wall if (wall_above := not self.edge_toward((x,0),self.UP)) else air}{wall}" for x,node in enumerate(self.grid[0]))
+        # Add to string row-wise
         for y,row in enumerate(self.grid):
-            string += f"\n{wall}"
+            string += f"\n{wall if (wall_left := not self.edge_toward((0,y),self.LEFT)) else air}"
             strbelow = f"\n{wall}"
+            # Add two cells per node
             for x,node in enumerate(row):
                 wall_right = not self.edge_toward((x,y),self.RIGHT)
-                wall_below = not self.edge_toward((x,y),self.DOWN)
                 string += f"{air}{wall if wall_right else air}"
+                wall_below = not self.edge_toward((x,y),self.DOWN)
                 strbelow += f"{wall if wall_below else air}{wall}"
             string += strbelow
         return string
@@ -108,8 +120,9 @@ class Maze:
 
 def main():
     maze = Maze(10,10)
+    print(maze)
     print(f"{maze}")
-    print(maze.ascii_str('%#'))
+    print(f"{maze:%#}")
 
 if __name__=="__main__": main()
 
