8cbbd2661a3fb3d4283d4be5935b33b0e483ceec
Strophox <strophox@gmail.com>
2023-07-21 05:17:40 +0200

    benchmark fixes 2

--- my_benchmark.py
+++ my_benchmark.py
@@ -17,6 +17,8 @@ import time
 
 # CONSTANTS BEGIN
 
+_2 = lambda n: (2**n, 2**n)
+
 SEQ_00 = [
       (r"BEGIN", lambda maze: maze
 
@@ -30,39 +32,39 @@ SEQ_01 = [
       (r"BEGIN", lambda maze: maze
 
     ),("maze  0", lambda maze:
-        Maze(*_2(0))
+        Maze(*__(0))
     ),("maze  1", lambda maze:
-        Maze(*_2(1))
+        Maze(*__(1))
     ),("maze  2", lambda maze:
-        Maze(*_2(2))
+        Maze(*__(2))
     ),("maze  3", lambda maze:
-        Maze(*_2(3))
+        Maze(*__(3))
     ),("maze  4", lambda maze:
-        Maze(*_2(4))
+        Maze(*__(4))
     ),("maze  5", lambda maze:
-        Maze(*_2(5))
+        Maze(*__(5))
     ),("maze  6", lambda maze:
-        Maze(*_2(6))
+        Maze(*__(6))
     ),("maze  7", lambda maze:
-        Maze(*_2(7))
+        Maze(*__(7))
     ),("maze  8", lambda maze:
-        Maze(*_2(8))
+        Maze(*__(8))
     ),("maze  9", lambda maze:
-        Maze(*_2(9))
+        Maze(*__(9))
     ),("maze 10", lambda maze:
-        Maze(*_2(10))
+        Maze(*__(10))
     ),("maze 11", lambda maze:
-        Maze(*_2(11))
+        Maze(*__(11))
     ),("maze 12", lambda maze:
-        Maze(*_2(12))
+        Maze(*__(12))
     ),("maze 13", lambda maze:
-        Maze(*_2(13))
+        Maze(*__(13))
     ),("maze 14", lambda maze:
-        Maze(*_2(14))
+        Maze(*__(14))
     ),("maze 15", lambda maze:
-        Maze(*_2(15))
+        Maze(*__(15))
     ),("maze 16", lambda maze:
-        Maze(*_2(16))
+        Maze(*__(16))
 
     ),(r"END", lambda maze: maze)
 ]
@@ -72,19 +74,19 @@ SEQ_02 = [
       (r"BEGIN", lambda maze: maze
 
     ),("random_edges", lambda maze:
-        Maze.random_edges(*_2(N))
+        Maze.random_edges(*__(N))
     ),("growing_tree", lambda maze:
-        Maze.growing_tree(*_2(N))
+        Maze.growing_tree(*__(N))
     ),("backtracker", lambda maze:
-        Maze.backtracker(*_2(N))
+        Maze.backtracker(*__(N))
     ),("prim", lambda maze:
-        Maze.prim(*_2(N))
+        Maze.prim(*__(N))
     ),("kruskal", lambda maze:
-        Maze.kruskal(*_2(N))
+        Maze.kruskal(*__(N))
     ),("wilson", lambda maze:
-        Maze.wilson(*_2(N))
+        Maze.wilson(*__(N))
     ),("division", lambda maze:
-        Maze.division(*_2(N))
+        Maze.division(*__(N))
 
     ),(r"END", lambda maze: maze)
 ]
@@ -93,11 +95,11 @@ SEQ_03 = [
       (r"BEGIN", lambda maze: maze
 
     ),("division", lambda maze:
-        Maze.division(*_2(9))
+        Maze.division(*__(9))
     ),("growing_tree (fast_pop)", lambda maze:
-        Maze.growing_tree(*_2(9),fast_pop=True)
+        Maze.growing_tree(*__(9),fast_pop=True)
     ),("wilson", lambda maze:
-        Maze.wilson(*_2(7))
+        Maze.wilson(*__(7))
     ),("SAVE image", lambda maze:
         maze.generate_image().save(f"{maze.generate_name()}.png") and()or maze # <- cursed sequential expression composition
     ),("make_unicursal", lambda maze:
@@ -112,7 +114,7 @@ SEQ_04 = [
       (r"BEGIN", lambda maze: maze
 
     ),("backtracker", lambda maze:
-        Maze.backtracker(*_2(10))
+        Maze.backtracker(*__(10))
     ),("breadth_first_search", lambda maze:
         maze.breadth_first_search() and()or maze
     ),("generate_solutionimage", lambda maze:
@@ -133,17 +135,6 @@ SEQ_CHOSEN = SEQ_04
 
 # FUNCTIONS BEGIN
 
-def _2(n):
-    """Produce a pair (2**n,2**n) given some number n."""
-    return (2**n,2**n)
-
-def run_and_time(f):
-    """Run a function and return its result and execution time as tuple."""
-    start_time = time.perf_counter()
-    result = f()
-    time_taken = time.perf_counter() - start_time
-    return (result, time_taken)
-
 def run_benchmark_on(sequence):
     """Runs and benchmarks a sequence of 'actions'.
 
@@ -152,8 +143,10 @@ def run_benchmark_on(sequence):
     """
     main_maze = None
     for (title,action) in sequence:
-        (main_maze, time_taken) = run_and_time(lambda:action(main_maze))
-        print(f"['{title}' completed in {time_taken:.03f}s]")
+        begin_time = time.perf_counter()
+        main_maze  = action(main_maze)
+        end_time   = time.perf_counter()
+        print(f"['{title}' completed in {end_time-begin_time:.03f}s]")
     return
 
 # FUNCTIONS END
